5fa420f9b9b27f84f617175db436f304e3dd73fc
Jimmy Gonzalez <jimmygonzalez87@gmail.com>
2016-12-13 14:11:23 -0800

    finally done

--- client/src/components/Map/Header.js
+++ client/src/components/Map/Header.js
@@ -2,18 +2,19 @@ import React, { Component } from 'react';
 import eat from '../../assets/eat_icon.svg';
 import drink from '../../assets/drink_icon.svg';
 import pin from '../../assets/marker_red.svg';
+import ldrBg from '../../assets/loader_bg.jpg';
 
 class Header  extends Component{
   render () {
-    const pinImg = this.props.selections.dinnerSelected 
-                                ? <img src={pin}/> 
+    const pinImg = this.props.selections.dinnerSelected
+                                ? <img src={pin}/>
                                 : null;
     const brandImg = this.props.selections.dinnerSelected ? drink : eat;
     return (
       <header className="map-header">
           <img className="brand" src={brandImg} alt=""/>
       	<div className="step first-step">
-      		Step 1: <span>{this.props.selections.dinnerVenue}</span> 
+      		Step 1: <span>{this.props.selections.dinnerVenue}</span>
               {pinImg}
       	</div>
       	<div className="step">
